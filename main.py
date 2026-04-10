16b6674d26aaba1090d315fe72c2774106cb8d43
Ch.Suhasini <suhasinichinni158@gmail.com>
2026-04-10 22:22:39 +1200

    add fibonacci

--- main.py
+++ main.py
@@ -25,3 +25,18 @@ def square(n: int):
         "input": n,
         "result": n * n
     }
+
+@app.get("/fibonacci")
+def fibonacci(n: int):
+    if n < 0:
+        return {"error": "Input must be a non-negative integer."}
+    elif n == 0:
+        return {"result": 0}
+    elif n == 1:
+        return {"result": 1}
+    
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return {"result": b}
+
